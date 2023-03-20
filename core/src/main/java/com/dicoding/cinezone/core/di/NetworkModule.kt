71d43f9151aac4d9ec6ef0c5ad72d3ef2ab4feb0
ilham arsy <ilhamarsy16@gmail.com>
2023-03-20 15:37:08 +0800

    update security

--- core/src/main/java/com/dicoding/cinezone/core/di/NetworkModule.kt
+++ core/src/main/java/com/dicoding/cinezone/core/di/NetworkModule.kt
@@ -12,6 +12,7 @@ import java.util.concurrent.TimeUnit
 import com.dicoding.cinezone.core.BuildConfig.BASE_URL
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -19,21 +20,29 @@ class NetworkModule {
 
     @Provides
     fun provideOkHttpClient(): OkHttpClient {
+        val certificatePinner = CertificatePinner.Builder()
+            .add(BASE_URL, "sha256/NPIMWkzcNG/MyZsVExrC6tdy5LTZzeeKg2UlnGG55UY=")
+            .add(BASE_URL, "sha256/DxH4tt40L+eduF6szpY6TONlxhZhBd+pJ9wbHlQ2fuw=")
+            .add(BASE_URL, "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+            .build()
+
         val loggingInterceptor = if (BuildConfig.DEBUG)
             HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
          else
             HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+
         return OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
 
     @Provides
     fun provideApiService(client: OkHttpClient): ApiService {
         val retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
+            .baseUrl("https://$BASE_URL/3/")
             .addConverterFactory(GsonConverterFactory.create())
             .client(client)
             .build()
